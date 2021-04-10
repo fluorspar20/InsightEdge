744858ae2633766d83edadf0acbe010e9499e928
fluorspar20 <amoghdixit104@gmail.com>
2021-04-10 20:20:33 +0530

    connected to mongodb

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const mongoose = require('mongoose')
+
+const userRouter = require('./routes/userRouter')
 
 const app = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use(cors())
 
 // routes
-app.use('/', (req, res, err) => {
-    res.send('Hello World')
+// app.use('/', (req, res, err) => {
+//     res.send('Hello World')
+// })
+app.use('/users', userRouter)
+
+//connect to mongodb
+const URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.tk5zd.mongodb.net/InsightEdge?retryWrites=true&w=majority`
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if (err)
+        throw err
+    console.log('Connected to MongoDB')
 })
 
 const PORT = process.env.PORT || 5000
