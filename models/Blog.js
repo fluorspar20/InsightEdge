131ad3236e6519d51936993d64a6dde7747b605e
fluorspar20 <amoghdixit104@gmail.com>
2021-04-21 23:47:28 +0530

    :package: added Blog Model

---
+++ models/Blog.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    read_time: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Blogs", blogSchema);
