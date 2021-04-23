e3e4ae1286c7562180c36dd8fba633f00dc97f78
fluorspar20 <amoghdixit104@gmail.com>
2021-04-23 21:48:52 +0530

    :ambulance: increased server payload to 50mb

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const userRouter = require("./routes/userRouter");
 const blogRouter = require("./routes/blogRouter");
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(cors());
 
