6d6da4349454c907e7c93e639534a999ce14acae
fluorspar20 <amoghdixit104@gmail.com>
2021-04-26 10:47:44 +0530

    updated to incorporate userCtrl

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,12 @@
 const userRouter = require("express").Router();
 const authCtrl = require("../controllers/auth");
+const userCtrl = require("../controllers/user");
 
 userRouter.post("/signup", authCtrl.signup);
 userRouter.post("/login", authCtrl.login);
-userRouter.post("/getUser", authCtrl.getUser);
-userRouter.post("/getUserById", authCtrl.getUserById);
+userRouter.post("/getUser", userCtrl.getUser);
+userRouter.post("/getUserById", userCtrl.getUserById);
+userRouter.post("/getUserAndBlogs", userCtrl.getUserAndBlogs);
+userRouter.put("/updateProfile", userCtrl.updateProfile);
 
 module.exports = userRouter;
